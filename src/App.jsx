3699931b42992fb4b13b0cedfe5b36e6842daaff
asherayub <asherayub52@gmail.com>
2023-03-24 21:41:58 +0500

    error fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
 import "./index.css";
-import FormData from "../../FormData";
 import Tenzies from "./components/Tenzies";
 function App() {
   return ( 
     <div>
-      {/* <FormData /> */}
       <Tenzies />
     </div>
   );
